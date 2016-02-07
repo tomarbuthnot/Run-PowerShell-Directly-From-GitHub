393db18ffe2b55c4e7a2032320f55c240edc9c80
Tom Arbuthnot <tom@tomarbuthnot.com>
2016-02-07 11:36:32 +0000

    Example 1

---
+++ Run-FromGitHub-SamplePowerShell.ps1
@@ -0,0 +1,18 @@
+﻿# Demo of PowerShell download and run directly from GitHub
+
+# Note, from a security point of view this is a really bad idea, you shouldn't run code
+# before reviewing it carefully and understanding it, and anyone who has access to the 
+# GitHub repo could  change the code at any time 
+# (which is a benefit for updating, but a risk for running it)
+
+# Anyway, don't do it. This is just a proof of concept :-)
+
+# Tom Arbuthnot tontalks.uk
+
+# You can run this script directly from GitHub with these two commands:
+
+Write-Host " "
+Write-Host "Here are the top 10 CPU consuming processes right now"
+Write-Host " "
+Get-Process | Sort-Object CPU -Descending | Select-Object -First 10
+
---
+++ Run-FromGithub.ps1
@@ -0,0 +1,5 @@
+﻿
+
+$Script = Invoke-WebRequest https://raw.githubusercontent.com/tomarbuthnot/Get-SfBClientVersion/master/Get-SfBClientVersion.ps1
+Invoke-Expression $($Script.Content)
+
